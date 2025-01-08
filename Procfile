8ef032f2be553538702946c5662eab282040da5e
teja <tejavenkatballa@gmail.com>
2025-01-08 17:52:09 +0530

    make it better

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn scanNhelp.wsgi:application --log-file -
--- scanNhelp/settings.py
+++ scanNhelp/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import django_heroku
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -148,3 +149,7 @@ SIMPLE_JWT = {
 }
 import os
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
+STATICFILES_DIRS=(os.path.join(BASE_DIR, 'static'),)
+django_heroku.settings(locals())
